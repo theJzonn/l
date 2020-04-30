fdf0e2c58a8ad2250ca3000d008c6c7a0f631ae0
theJzonn <jzonnmail@gmail.com>
2020-04-30 15:40:51 +0200

    update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,22 +6,22 @@
 /*   By: jricafor <jricafor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 22:52:31 by jricafor          #+#    #+#             */
-/*   Updated: 2020/04/27 22:52:34 by jricafor         ###   ########.fr       */
+/*   Updated: 2020/04/30 15:25:38 by jricafor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft.h"
 
-char			*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
+	unsigned	int	i;
+	unsigned	int	j;
 	char			*res;
-	unsigned int	i;
-	unsigned int	j;
-
 
 	if (!s1 || !s2)
 		return (NULL);
-	if (!(res = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
+	if (!(res = (char *)malloc(sizeof(char) *
+		(ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (NULL);
 	i = 0;
 	j = 0;
