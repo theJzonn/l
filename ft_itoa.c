7ce5d497d2eff1bd7afdeaed5e1a692dfa9f67e8
theJzonn <jzonnmail@gmail.com>
2020-04-30 15:06:56 +0000

    update

--- ft_itoa.c
+++ ft_itoa.c
@@ -12,35 +12,48 @@
 
 #include "./libft.h"
 
-static size_t	get_str_len(int n)
+static size_t	ft_len(long nb)
 {
-	size_t		i;
+	size_t	size;
 
-	i = 1;
-	while (n /= 10)
-		i++;
-	return (i);
+	size = 0;
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		size = 1;
+	}
+	if (nb / 10 == 0)
+		return (size + 1);
+	else
+		nb = nb / 10;
+	return (size + ft_len(nb) + 1);
 }
 
 char			*ft_itoa(int n)
 {
-	char			*str;
-	size_t			str_len;
-	unsigned int	n_cpy;
+	char			*res;
+	unsigned int	i;
+	size_t			len;
+	long			nb;
 
-	str_len = get_str_len(n);
-	n_cpy = n;
-	if (n < 0)
-	{
-		n_cpy = -n;
-		str_len++;
-	}
-	if (!(str = malloc(sizeof(char) * str_len)))
+	nb = n;
+	len = ft_len(nb);
+	if (!(res = (char *)malloc(sizeof(*res) * (len + 1))))
 		return (NULL);
-	str[--str_len] = n_cpy % 10 + '0';
-	while (n_cpy /= 10)
-		str[--str_len] = n_cpy % 10 + '0';
-	if (n < 0)
-		*(str + 0) = '-';
-	return (str);
+	res[len--] = '\0';
+	if (nb < 0)
+	{
+		nb = nb * -1;
+		res[0] = '-';
+	}
+	if (nb == 0)
+		res[len] = '0';
+	i = 0;
+	while (i <= len && nb > 0)
+	{
+		res[len - i] = nb % 10 + 48;
+		nb = nb / 10;
+		i++;
+	}
+	return (res);
 }
