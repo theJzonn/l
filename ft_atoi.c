b2aa67fb8c6cb83f3d0e7a5f0559a6fa44806bcd
theJzonn <jzonnmail@gmail.com>
2020-05-01 19:16:57 +0000

    updaet

--- ft_atoi.c
+++ ft_atoi.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "./libft.h"
+
 int		ft_atoi(char *str)
 {
 	int nb;
@@ -27,10 +29,14 @@ int		ft_atoi(char *str)
 			negatif = -1;
 		i++;
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (str[i] && ft_isdigit(str[i]))
 	{
 		nb = nb * 10 + str[i] - 48;
 		i++;
 	}
+	if (nb < 0 && negatif > 0)
+		return (-1);
+	else if (nb < 0 && negatif < 0 && nb * negatif != -2147483648)
+		return (0);
 	return (nb * negatif);
 }
