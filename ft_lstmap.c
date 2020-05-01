ff010855c1aad7f5c22aebb2600a090672dff745
Jason RICAFORT <jricafor@e3r6p8.42.fr>
2020-05-01 23:04:55 +0200

    update

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: jricafor <jricafor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 22:51:05 by jricafor          #+#    #+#             */
-/*   Updated: 2020/05/01 22:55:15 by jricafor         ###   ########.fr       */
+/*   Updated: 2020/05/01 23:01:35 by jricafor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list *new;
 	t_list *res;
 
+	(void)(*del);
 	if (!lst)
 		return (NULL);
 	new = ft_lstnew((*f)(lst->content));
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jricafor <jricafor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 17:22:58 by jricafor          #+#    #+#             */
-/*   Updated: 2020/05/01 22:48:17 by jricafor         ###   ########.fr       */
+/*   Updated: 2020/05/01 22:59:25 by jricafor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,5 +66,6 @@ void			ft_lstadd_back(t_list **alst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void(*f)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
