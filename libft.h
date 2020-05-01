e486e9191590e3d5b69cc13067d6391d0da75b9c
Jason RICAFORT <jricafor@e3r6p8.42.fr>
2020-05-01 23:18:46 +0200

    update

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jricafor <jricafor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/27 17:22:58 by jricafor          #+#    #+#             */
-/*   Updated: 2020/05/01 22:59:25 by jricafor         ###   ########.fr       */
+/*   Updated: 2020/05/01 23:13:19 by jricafor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,7 @@ void			ft_lstadd_back(t_list **alst, t_list *new);
 void			ft_lstdelone(t_list *lst, void (*del)(void *));
 void			ft_lstclear(t_list **lst, void (*del)(void *));
 void			ft_lstiter(t_list *lst, void(*f)(void *));
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void *)
+		, void (*del)(void *));
 
 #endif
